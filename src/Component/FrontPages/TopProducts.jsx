b963d794e83a1f8e6fa980dcd19e2335b6f87366
Farjana Eti <etimourakkhieti@gmail.com>
2025-07-05 13:37:01 +0600

    top rating product

--- src/Component/FrontPages/TopProducts.jsx
+++ src/Component/FrontPages/TopProducts.jsx
@@ -6,6 +6,9 @@ import { motion as _motion } from "framer-motion";
 const TopProducts = () => {
   const [products] = useProducts(); 
   console.log(products);
+   const topSix = [...products]
+    .sort((a, b) => parseFloat(b.rating) - parseFloat(a.rating)) // descending
+    .slice(0, 6); 
 
   return (
     <div className="px-4 md:px-10 py-8  min-h-screen">
@@ -18,7 +21,7 @@ const TopProducts = () => {
 </_motion.h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products?.map(product => (
+        {topSix?.map(product => (
           <div
             key={product._id}
             className="bg-gradient-to-r from-pink-200 to-emerald-200 rounded-xl shadow-md overflow-hidden hover:shadow-lg transition duration-300"
@@ -34,6 +37,10 @@ const TopProducts = () => {
 
               <p className="text-sm font-medium text-green-600 mt-2">
                 Category: <span className="text-gray-700">{product.category}</span>
+              </p>
+              <p className="text-sm font-medium text-green-600 mt-2">
+                
+            Rating: <span className="text-gray-700">{product.rating}</span>
               </p>
 
               <div className="flex justify-between items-center mt-4">
